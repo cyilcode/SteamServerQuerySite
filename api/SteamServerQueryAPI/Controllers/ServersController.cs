1580df2d389108866b6842c1fa435087720c4f2a
cyilcode <cmylmzbm@outlook.com>
2018-08-13 21:52:13 +0300

    Initial API push

---
+++ api/SteamServerQueryAPI/Controllers/ServersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteamQueryNet;
+using System.Threading.Tasks;
+
+namespace SteamServerQueryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServersController : ControllerBase
+    {
+        private readonly IServerQuery _serverQuery;
+
+        public ServersController(IServerQuery serverQuery)
+        {
+            _serverQuery = serverQuery;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult ServerInfo([FromQuery]string ip, [FromQuery]int port)
+        {
+            _serverQuery.Connect(ip, port);
+            return Ok(_serverQuery.GetServerInfo());
+        }
+    }
+}
